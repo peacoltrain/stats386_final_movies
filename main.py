eb3b6e80b468ff952f4fd605dde1f3cc8eab17f1
Isaac Keenan <peacoltrain@gmail.com>
2025-12-08 09:04:12 -0700

    Basic Initialization

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from stats386-final-movies!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/stats386_final_movies/__init__.py
@@ -0,0 +1,7 @@
+#This will be the file in which we can make our pipline and other functions available
+
+#use formate from .scriptName import listOfFunctions
+
+#Add string of function name to __all__ when ready to add
+__all__ = [
+]
